25a5ce4f7f34a819cb8d81296665d555d94ed6c9
Shaukat Sohail <sohailriazz267@gmail.com>
2021-10-08 16:06:33 +0500

    Connected to MONGOdb ATLAST

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express =require("express");
 const app=express();
+const mongoose=require("mongoose");
 
+const DB='mongodb+srv://Shaukat:31429860@cluster0.7tqxz.mongodb.net/MernStack?retryWrites=true&w=majority';
+
+
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    // useCreateIndex:true,
+    // useUnifiedTopology:true,
+    // useFindAndModify:false
+}).then(()=>{
+    console.log("connected to DB")
+    
+}).catch((E)=>{
+    console.log(E,"error")
+})
 
 const MiddleWare=(req,res,next)=>{
     console.log("middle ware");
