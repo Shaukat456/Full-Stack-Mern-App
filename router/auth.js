afd48183150f3dc6e1b097946dd47b191827267a
Shaukat Sohail <sohailriazz267@gmail.com>
2021-10-09 14:09:05 +0500

    BCRYPT JS

--- router/auth.js
+++ router/auth.js
@@ -8,7 +8,10 @@ router.get("/",(req,res)=>{
     res.send("hello world from auth")
     
 })
-router.post("/register",(req,res)=>{
+
+
+
+router.post("/register", (req,res)=>{
    
     const {name ,email,password,confpassword}=req.body
     
@@ -16,17 +19,20 @@ router.post("/register",(req,res)=>{
 if( !name ||  !email  || !password   ||  !confpassword         ){
     return res.send("please fill all the fields")
 }
+
+
     
     
-    User.findOne({email:email}).then((UserExists)=>{
+   User.findOne({email:email}).then((UserExists)=>{
      if(UserExists){
          return res.status(422).json({error:"Email already exists"})
      }
 
      const user=new User({ name ,email,password,confpassword  })
-      user.save().then(()=>{
-          console.log("user created")
-          return res.send("created")
+      user.save().then((u)=>{
+          //   console.log("user created")
+          return [console.log(u),res.send(u)]
+          
       }).catch((e)=>{
           console.log(e)
       }).catch((er)=>{
@@ -40,5 +46,26 @@ if( !name ||  !email  || !password   ||  !confpassword         ){
 })
 
 
+// Login Route
+
+router.post("/signin",async(req,res)=>{
+    // console.log(req.body)
+try{
+    const {email,password}=req.body;
+
+    if(!email || !password ){
+        return res.status(400).json({error:"LOGIN ERROR"})
+        
+    }
+
+    const UserLogin= await User.findOne({email:email});
+    console.log(UserLogin)
+
+}
+catch(err){
+    console.log('error')
+}
+
+})
 
 module.exports=router;
