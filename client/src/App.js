81230cbaf89cd738cbaa92217ba73eade563c6b8
Shaukat Sohail <sohailriazz267@gmail.com>
2021-10-11 15:34:51 +0500

    SENDING DATA TO BACKEND

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import Contact from "./Components/Contact";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
   <>
   <Navbar/>
-  
+  <Contact/>
 
   </>
   );
--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -1,9 +1,56 @@
+import { useState } from "react";
+
 function Contact() {
+    const [Visitor,setVisitor]=useState({
+        name:"",email:"",password:"",contact:"",
+    });
+
+    function HandleInputs(e){
+        setVisitor(e.target.value)
+        console.log(e.target.value)
+    }
+    function Submission(b){
+        b.preventDefault()
+    }
+
+
     return (
         
         <>
-        
-        
+
+<form>
+  <div class="form-group ">
+    <label for="staticEmail" class="col-sm-2 col-form-label">NAME</label>
+    <div class="col-sm-10">
+      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value={Visitor.name}   onChange={HandleInputs}  />
+    </div>
+  </div>
+  <div class="form-group col">
+    <label for="inputPassword" class="col-sm-2 col-form-label">EMAIL</label>
+    <div class="col-sm-5">
+      <input type="email" class="form-control" id="inputPassword"  onChange={HandleInputs} value={Visitor.email}   placeholder="email" />
+    </div>
+    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-5">
+      <input type="password" class="form-control" id="inputPassword"  onChange={HandleInputs}  value={Visitor.password}  placeholder="Password" />
+    </div>
+    <label for="inputPassword" class="col-sm-2 col-form-label"> Contact</label>
+    <div class="col-sm-5">
+      <input type="password" class="form-control" id="inputPassword"  onChange={HandleInputs} value={Visitor.contact}  placeholder="Contact" />
+    </div>
+    <button type="submit" class="btn btn-info mx-auto my-5" onClick={Submission}  >Submit</button>
+
+   
+  </div>
+</form>
+
+    {/* <div className="container">
+
+    </div>
+       <input type="text"  value='Enter Name ' />
+       <input type="text"  value='Enter  Email ' />
+       <input type="text"  value='Enter Contact ' />
+       <input type="text"  value=' Phone' /> */}
         </>
 
 
